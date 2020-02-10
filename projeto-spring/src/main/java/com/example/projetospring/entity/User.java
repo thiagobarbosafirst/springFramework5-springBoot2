92fb84fb0da00c6688b4514a3d8261c7fb53f5fc
Thiago Barbosa <thiago-barbosa@dsn028300-Matias.cobra.com.br>
2020-02-10 16:14:32 -0300

    JPA em diversos bancos

---
+++ projeto-spring/src/main/java/com/example/projetospring/entity/User.java
@@ -0,0 +1,49 @@
+package com.example.projetospring.entity;
+
+import java.util.Set;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+
+@Entity
+public class User {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	
+	private String name;
+	
+	private String email;
+	
+	@ManyToMany
+	private Set<Role> roles;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Set<Role> getRoles() {
+		return roles;
+	}
+
+	public void setRoles(Set<Role> roles) {
+		this.roles = roles;
+	}
+
+}
