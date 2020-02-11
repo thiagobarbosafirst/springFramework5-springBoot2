9669b6b4b8995c2b6e0324762da57ae52640c413
Thiago Barbosa <thiago-barbosa@dsn028300-Matias.cobra.com.br>
2020-02-11 16:03:18 -0300

    mais sobre JPA

--- projeto-spring/src/main/java/com/example/projetospring/config/DataInitializr.java
+++ projeto-spring/src/main/java/com/example/projetospring/config/DataInitializr.java
@@ -1,10 +1,13 @@
 package com.example.projetospring.config;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
 
+import com.example.projetospring.entity.User;
 import com.example.projetospring.repository.UserRepository;
 
 @Component
@@ -15,7 +18,20 @@ public class DataInitializr implements ApplicationListener<ContextRefreshedEvent
 	
 	@Override
 	public void onApplicationEvent(ContextRefreshedEvent event) {
-		// TODO Auto-generated method stub
+		List<User> users = userRepository.findAll();
+		
+		if(users.isEmpty()) {
+			User user = new User();
+			
+			user.setName("Thiago Barbosa");
+			user.setEmail("thiago.barbosa@bbts.com.br");
+			
+			userRepository.save(user);
+		}
+		
+		User user = userRepository.findByEmail("thiago.barbosa@bbts");
+		
+		System.out.println(user.getName());
 		
 	}
 
--- projeto-spring/src/main/java/com/example/projetospring/repository/UserRepository.java
+++ projeto-spring/src/main/java/com/example/projetospring/repository/UserRepository.java
@@ -6,4 +6,6 @@ import com.example.projetospring.entity.User;
 
 public interface UserRepository extends JpaRepository<User, Long>{
 	
+	User findByEmail(String email);
+	
 }
