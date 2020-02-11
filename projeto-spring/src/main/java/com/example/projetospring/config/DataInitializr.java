950b33b1ae2168f3da3c68e016af2f35649f217a
thiagobarbosafirst <tmbthiagobarbosa@gmail.com>
2020-02-11 00:39:22 -0300

    Melhorias

--- projeto-spring/src/main/java/com/example/projetospring/config/DataInitializr.java
+++ projeto-spring/src/main/java/com/example/projetospring/config/DataInitializr.java
@@ -1,13 +1,18 @@
 package com.example.projetospring.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationListener;
-import org.springframework.context.event.ApplicationListenerMethodAdapter;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
+
+import com.example.projetospring.repository.UserRepository;
 
 @Component
 public class DataInitializr implements ApplicationListener<ContextRefreshedEvent>{
 
+	@Autowired
+	UserRepository userRepository;
+	
 	@Override
 	public void onApplicationEvent(ContextRefreshedEvent event) {
 		// TODO Auto-generated method stub
